1765846d8aa910c257c61e9812e24373645e20ef
Magdalena Gola <turcza.magdalena@gmail.com>
2020-09-22 11:41:29 +0200

    Add solution to FlatMultiDimensionalList task

--- src/main/java/com/github/wojtechm/zadania_rekrutacyjne/arrays_and_lists/FlatMultidimensionalList.java
+++ src/main/java/com/github/wojtechm/zadania_rekrutacyjne/arrays_and_lists/FlatMultidimensionalList.java
@@ -3,12 +3,14 @@ package com.github.wojtechm.zadania_rekrutacyjne.arrays_and_lists;
 import com.github.wojtechm.zadania_rekrutacyjne.tools.Difficulty;
 import com.github.wojtechm.zadania_rekrutacyjne.tools.Level;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Given a list of list of strings
  * write a method that will take all elements of inner lists, and put them into single list
- *
+ * <p>
  * [ ["1", "2"], ["3"], [], ["4", "5", "6"] ]       => ["1", "2", "3", "4", "5", "6"]
  *
  * @author Wojciech Makieła
@@ -17,6 +19,9 @@ import java.util.List;
 class FlatMultidimensionalList {
 
     static List<String> flat(List<List<String>> list) {
-        return null;
+        if (list == null) throw new IllegalArgumentException();
+        return list.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 }
